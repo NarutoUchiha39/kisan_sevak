bc96ee023282cffca541703f6a1a923074480c5c
NarutoUchiha39 <anirudhata123@gmail.com>
2023-09-29 11:20:38 +0530

    Tried fixing db issues

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ let connection;
 let connection2 = null
 
   if(connection2==null)
-   connection =  mongoose.connect(process.env.ATLAS_URI,{
+   connection =  mongoose.connect(process.env.MONGODB_URI,{
     dbName:'SIH2023_Final'
 }) .then(console.log('DB connected'))
 .catch("DB not connected :(")
